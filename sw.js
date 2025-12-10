f2524eb8eb3f5c223bb2aaaf0fd03f1014b144fe
benalee <abdulkareemaleeu@gmail.com>
2025-12-10 15:28:27 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('windaily').then(cache => cache.addAll(['/', 'index.html', 'style.css']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
